097214cd5a4396544b069f842bcab39b5e6a9e2f
Priyanshu14G <pgupta0628@gmail.com>
2025-06-11 22:58:17 +0530

    dashboard update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
+    <Link herf="/dashboard">
       <Button>Let's go</Button>
+    </Link>
     </div>
   );
 }
