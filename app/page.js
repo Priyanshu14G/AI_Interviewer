d0d72f655c81a13e205e4b8e7f0dda77ded3bd2d
Priyanshu14G <pgupta0628@gmail.com>
2025-06-11 23:18:50 +0530

    button update

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-    <Link herf="/dashboard">
+    <Link href='/dashboard'>
       <Button>Let's go</Button>
     </Link>
     </div>
